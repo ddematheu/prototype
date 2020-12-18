7fce039e141df2d67da71498b4d42d2208ed75ce
KYLO\ddema <david.dematheu@squadventure.com>
2020-12-18 06:18:10 -0800

    Updates

---
+++ component---src-pages-404-tsx-ef406ecd88918e8a8717.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{"i6+/":function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),u=n.n(a),s=n("9Dj+");t.default=function(){return u.a.createElement(s.b,null,u.a.createElement("h1",null,"NOT FOUND"),u.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-ef406ecd88918e8a8717.js.map
